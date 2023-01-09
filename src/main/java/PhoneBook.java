385a2340a7a9da06134e8ad328a3c78e9bd07bac
Limpsobaka <limpsobaka@yandex.ru>
2023-01-09 20:00:08 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,22 @@
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class PhoneBook {
+  private static PhoneBook INSTANCE;
+  private final SortedMap<String, String> entrys = new TreeMap<>();
+
+  private PhoneBook() {
+  }
+
+  public static PhoneBook getINSTANCE() {
+    if (INSTANCE == null) {
+      INSTANCE = new PhoneBook();
+    }
+    return INSTANCE;
+  }
+
+  public int add(String name, String number) {
+    return 0;
+  }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,34 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+  PhoneBook phoneBook = PhoneBook.getINSTANCE();
+  @Test
+  public void testAdd() {
+    //assert
+    int expected = 2;
+    int result;
+
+    //act
+    phoneBook.add("Petya", "79102356789");
+    result = phoneBook.add("Vasya", "79102356489");
+
+    //assert
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void testAddDublicatedNames() {
+    //assert
+    int expected = 2;
+    int result;
+
+    //act
+    phoneBook.add("Petya", "79102356789");
+    phoneBook.add("Petya", "79252356782");
+    result = phoneBook.add("Vasya", "79102356489");
+
+    //assert
+    assertEquals(expected, result);
+  }
 }
